62e756aa8b859b7f93623cfbce0e8eebe031e239
krevanth <revanth91kamaraj@gmail.com>
2016-09-06 21:59:53 +0530

    thumb updates

--- rtl/zap_decode/zap_decode_thumb.v
+++ rtl/zap_decode/zap_decode_thumb.v
@@ -47,16 +47,53 @@ begin
         if ( i_cpsr_ff[T] && i_instruction_valid ) // Thumb mode.
         begin
                 casez ( i_instruction[15:0] )
-                       T_BRANCH_COND: decode_conditional_branch; 
+                        T_BRANCH_COND   : decode_conditional_branch; 
+                        T_BRANCH_NOCOND : decode_unconditional_branch;
+                        T_BL            : decode_bl;
+                        T_BX            : decode_bx;
                 endcase 
         end
 end
 
 task decode_conditional_branch;
 begin
-        o_instruction           = 1'd1, 2'b0, AL, 3'b101, 1'b0, 24'd0}; 
-        o_instruction[23:1]     = $signed(i_instruction[7:0]); // Left shift by 1 for HWA.
+        // An MSB of 1 indicates a left shift of 1.
+        o_instruction           = {1'd1, 2'b0, AL, 3'b101, 1'b0, 24'd0}; 
+        o_instruction[23:0]     = $signed(i_instruction[7:0]); 
 end        
+endtask
+
+task decode_unconditional_branch;
+begin
+        // An MSB of 1 indicates a left shift of 1.
+        o_instruction           = {1'd1, 2'b0, AL, 3'b101, 1'b0, 24'd0}; 
+        o_instruction[23:0]     = $signed(i_instruction[10:0]);        
+end
+endtask
+
+task decode_bl; 
+begin
+        case ( i_instruction[11] )
+        1'd1:
+        begin
+                o_instruction  = {1'd1, 2'b0, AL, 3'b101, 1'b1, 24'd0}; 
+                o_instruction[23:0] = $signed(i_instruction[10:0] << 10);               
+        end 
+        1'd0:
+        begin
+                o_instruction  = {1'd1, 2'b0, AL, 3'b101, 1'b1, 24'd0}; 
+                o_instruction[23:0] = $signed(i_instruction[10:0]);
+        end
+        endcase
+end
+endtask
+
+task decode_bx;
+begin
+        case ( i_instruction[6] )
+        
+        endcase 
+end
 endtask
 
 endmodule
