7d9a27bf8e55c5e95e7a78bdf24d251c8fdef802
krevanth <revanth91kamaraj@gmail.com>
2016-09-24 14:17:05 +0530

    added long mul

--- push_repo.pl
+++ push_repo.pl
@@ -5,7 +5,7 @@ die "*E: Failed to perform GIT ADD...\n" if system('git add .');
 print "*I: Enter commit message...\n";
 my $message = <STDIN>;
 
-die "*E: Failed to perform GIT COMMIT...\n" if system("git commit -m \"$message\"");
+system("git commit -m \"$message\"");
 die "*E: Failed to push changes to master...\n" if system('git push origin master');
 
 print "All OK...\n";
